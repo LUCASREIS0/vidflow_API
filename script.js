72b933384497f230cb78c599b32b821529a7143c
lucas reis <reis83212@gmail.com>
2024-04-11 11:06:03 -0300

    Tratamento de Erros com .catch()

--- script.js
+++ script.js
@@ -15,4 +15,7 @@ const api = fetch("http://localhost:3000/videos")
         </li>
         `;
     })
-);
+)
+.catch((error) => {
+    containerVideos.innerHTML = `<p> Houve um erro ao carregar os vídeos: ${error} </p>`;
+})
