36181775d682274de951198eed1475ccf13480d4
lucas reis <reis83212@gmail.com>
2024-04-13 22:44:27 -0300

    Ajuste e Detalhamento dos comentarios 🤌

--- script.js
+++ script.js
@@ -5,20 +5,20 @@ const containerVideos = document.querySelector(".videos__container");
 async function buscarEMostrarVideos(){
 
     // Para trabalhar com de tratamento de erros, usaremos um recurso chamado Try/Catch, que basicamente serão dois blocos.
-    // Depois do bloco de try{}, adicionaremos um catch(error){}
+    //Try (Tente): O bloco try contém o código que pode gerar uma exceção. Se ocorrer uma exceção dentro deste bloco, o controle do programa será transferido imediatamente para o bloco catch.
     try{
 
         // Faz uma requisição usando fetch para obter os vídeos da URL http://localhost:3000/videos
         //Esse await significa aguardar. Assim, ele aguardará que esta busca seja realizada para que o código desta função assíncrona continue sendo executado.
         const busca = await fetch("http://localhost:3000/videos");
 
-            // Converte a resposta em JSON quando a promessa é resolvida
+            // await response.json() espera que esta Promise seja resolvida antes de prosseguir. O JSON será atribuído à variável videos, que será utilizada posteriormente no código.
             const videos = await busca.json();
 
                 // Para cada vídeo retornado, cria um item de lista HTML com as informações do vídeo.containerVideos.innerHTML
                 videos.forEach((video) => {
 
-                    // tratamento de erros personalizado. se a categoria for vazia, usaremos o throw new Error(). Entre os parênteses, exibiremos uma mensagem, que é "Vídeo sem categoria".
+                    // tratamento de erros personalizado. se a categoria for vazia, usaremos o throw new Error() tradução => "lançar novo Error()". Entre os parênteses, exibiremos uma mensagem, que é "Vídeo sem categoria".
                     if(video.categoria == ""){
                         throw new Error('Vídeo não tem categoria');
                     }
@@ -35,7 +35,7 @@ async function buscarEMostrarVideos(){
                     </li>
                     `;
                 })
-    // recurso Try/Catch,            
+    //Catch (Capturar): O bloco catch é usado para lidar com a exceção que ocorreu no bloco try. Ele especifica o código que será executado para lidar com a exceção. O bloco catch recebe um parâmetro que representa o objeto de erro lançado.     
     } catch(error){
         containerVideos.innerHTML = `<p> Houve um erro ao carregar os vídeos: ${error}</p>`
     }   
